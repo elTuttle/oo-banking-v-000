65988aee2054c37422be10bde53d4317348ac98c
John Tuttle <thejwt@gmail.com>
2017-09-30 04:16:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid? && @status == "pending"
+    if @sender.valid? == true && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
