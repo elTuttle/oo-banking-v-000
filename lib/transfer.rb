bbadb92483927aeaf629d0035ee0f3128a70764b
John Tuttle <thejwt@gmail.com>
2017-09-30 04:18:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif !@sender.valid?
+    else
       "Transaction rejected. Please check your account balance."
     end
   end
