3f6957c9d79f6e1f516d16a35ab0a75902549aa5
John Tuttle <thejwt@gmail.com>
2017-09-30 04:04:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+    
+  end
 end
